ee97eaaf0e5f9755a6914ffc84923ab592d011b5
Asadbek <asadbeknan5566@gmail.com>
2024-05-25 16:31:45 +0500

    core changed

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,32 @@
 import { AuthLayout, HomeLayout } from "@/layouts"
-import { Home, Login, News } from "@/pages"
+import { Home, News, Login } from "./lazy"
 import { Routes, Route } from "react-router-dom"
+import { Suspense } from "react"
 
 const Router = () => {
   return (
     <>
       <Routes>
         <Route element={<HomeLayout />}>
-          <Route index path="/" element={<Home />} />
-          <Route path="/news" element={<News />} />
+          <Route index path="/" element={
+            <Suspense>
+              <Home />
+            </Suspense>
+          } />
+          <Route path="/news" element={
+            <Suspense>
+              <News />
+            </Suspense>
+          } />
         </Route>
       </Routes>
       <Routes>
         <Route element={<AuthLayout />}>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={
+            <Suspense>
+              <Login />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </>
---
+++ src/router/lazy.jsx
@@ -0,0 +1,29 @@
+import Loader from "@/components/shared/loader";
+import { lazy } from "react";
+
+const handleCatchChunkError = () => {
+  window.location.reload();
+  return { default: Loader };
+};
+
+const Home = lazy(() =>
+  import("@/pages")
+    .then(({ Home }) => ({ default: Home }))
+    .catch(handleCatchChunkError)
+);
+
+
+const News = lazy(() =>
+  import("@/pages")
+    .then(({ News }) => ({ default: News }))
+    .catch(handleCatchChunkError)
+);
+
+
+const Login = lazy(() =>
+  import("@/pages")
+    .then(({ Login }) => ({ default: Login }))
+    .catch(handleCatchChunkError)
+);
+
+export { Home, News, Login }
