d10623d7936dcba8ea5a676f540b59da46de8853
Asadbek <asadbeknan5566@gmail.com>
2024-05-25 15:20:35 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ModeToggle } from './components/shared/toggle-theme'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/index.css'
+import { ThemeProvider } from './components/shared/theme-provider/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme='dark' storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
