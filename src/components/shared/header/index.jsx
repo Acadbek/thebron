52ef17d6e910b5f936fec856418895c9114383d5
Asadbek <asadbeknan5566@gmail.com>
2024-05-25 17:10:54 +0500

    style: main padding

--- src/components/shared/header/index.jsx
+++ src/components/shared/header/index.jsx
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { ModeToggle } from "../toggle-theme";
 import { useTranslation } from "react-i18next";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import globus from '@/assets/icons/globus.svg'
 
 const Header = () => {
   const { i18n } = useTranslation();
+  const [currentLang, setCurrentLang] = useState('Uzbekcha')
 
-  const changeLanguage = (lng) => {
+  const changeLanguage = (e, lng) => {
     i18n.changeLanguage(lng);
+    setCurrentLang(e.target.textContent)
   };
 
   return (
@@ -19,10 +26,22 @@ const Header = () => {
         <NavLink to='/news'>News</NavLink>
         <NavLink to='/login'>Login</NavLink>
       </div>
-      <div flex gap-4>
-        <button onClick={() => changeLanguage('uz')}>Uzb</button>
-        <button onClick={() => changeLanguage('ru')}>Rus</button>
-      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button flex flex-col variant="ghost" size="icon">
+            <img src={globus} alt="" />
+            {currentLang}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={(e) => changeLanguage(e, "uz")}>
+            Uzbekcha
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={(e) => changeLanguage(e, "ru")}>
+            Русский
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   )
 }
