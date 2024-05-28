7eac59979eb0dd1705fc569c79bc98090d110198
nodirbekazamov <u900.nodir@gmail.com>
2024-05-28 16:04:57 +0500

    icons changed to component

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,52 @@
-import { AppleIcon } from "./appleIcon";
-
+import { AppleIcon } from "./AppleIcon";
+import { CalendarBlankIcon } from "./CalendarBlankIcon";
+import { CaretRightIcon } from "./CaretRightIcon";
+import { Chayxana } from "./Chayxana";
+import { Dacha } from "./Dacha";
+import { Djostilk } from "./Djostik";
+import { DoorOpenIcon } from "./DoorOpenIcon";
+import { FacebookIcon } from "./FacebookIcon";
+import { Filter } from "./Filter";
+import { Kino } from "./Kino";
+import { Globus } from "./Globus";
+import { GoogleIcon } from "./GoogleIcon";
+import { Heart } from "./Heart";
+import { Lager } from "./Lager";
+import { Logo } from "./Logo";
+import { OtelIcon } from "./OtelIcon";
+import { Pilot } from "./Pilot";
+import { Poezd } from "./Poezd";
+import { Restoran } from "./Restoran";
+import { Sanatoriya } from "./Sanatoriya";
+import { Search } from "./Search";
+import { StarIcon } from "./StarIcon";
+import { StarIcon2 } from "./StarIcon2";
+import { VectorIcon } from "./VectorIcon";
+import { WifiIcon } from "./WifiIcon";
 export {
-    AppleIcon
+    AppleIcon,
+    CalendarBlankIcon,
+    CaretRightIcon,
+    Chayxana,
+    Dacha,
+    Djostilk,
+    DoorOpenIcon,
+    FacebookIcon,
+    Filter,
+    Kino,
+    Globus,
+    GoogleIcon,
+    Heart,
+    Lager,
+    Logo,
+    OtelIcon,
+    Pilot,
+    Poezd,
+    Restoran,
+    Sanatoriya,
+    Search,
+    StarIcon,
+    StarIcon2,
+    VectorIcon,
+    WifiIcon,
 }
