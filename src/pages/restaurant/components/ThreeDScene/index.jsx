c356d29d5faa5d6beaff4a38440f265c9221abb5
Asadbek <asadbeknan5566@gmail.com>
2024-08-09 02:25:16 +0500

    feat: chairs change position with animation

--- src/pages/restaurant/components/ThreeDScene/index.jsx
+++ src/pages/restaurant/components/ThreeDScene/index.jsx
@@ -2,27 +2,24 @@ import React, { Suspense } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { a, useSpring } from "@react-spring/three";
 import { SceneContext } from "../../context";
 
 const ChairModel = ({ chair }) => {
   const gltf = useLoader(GLTFLoader, "/public/chair.glb");
   const clone = React.useMemo(() => gltf.scene.clone(), [gltf]);
 
-  return (
-    <primitive
-      rotation={chair.rotation}
-      object={clone}
-      position={chair.position}
-      scale={9}
-    />
-  );
+  const props = useSpring({
+    position: chair.position,
+    rotation: chair.rotation,
+    config: { mass: 1, tension: 170, friction: 26 },
+  });
+
+  return <a.primitive object={clone} scale={9} {...props} />;
 };
 
 const TableModel = () => {
   const gltf = useLoader(GLTFLoader, "/public/table.glb");
-
-  console.log(gltf);
-
   return <primitive object={gltf.scene} position={[0, 0, 0]} scale={4} />;
 };
 
@@ -51,10 +48,9 @@ const Scene = () => {
     return positions;
   };
 
-  const numChairs = chair;
   const radius = 3.7;
 
-  const chairsPositions = generateChairPositions(numChairs, radius);
+  const chairsPositions = generateChairPositions(chair, radius);
 
   return (
     <Suspense fallback={null}>
@@ -72,7 +68,7 @@ const ThreeDScene = () => {
   return (
     <Canvas
       camera={{ position: [0, 5, 10], fov: 50, near: 0.1, far: 1000 }}
-      style={{ height: "100vh", background: "#fff" }}
+      style={{ height: "100%", background: "#fff", cursor: "grab" }}
     >
       <OrbitControls enableZoom={true} enableRotate={true} />
       <Scene />
