69b6535711cb58dc335f3b2fe00b3ad511614f1b
Asadbek <asadbeknan5566@gmail.com>
2024-08-10 20:46:05 +0500

    feat: clicking on table object and zooming into it

--- src/pages/restaurant/components/TableScene.jsx
+++ src/pages/restaurant/components/TableScene.jsx
@@ -1,7 +1,9 @@
 import React, { Suspense, useMemo } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls, Html } from "@react-three/drei";
+import { OrbitControls, Html, Bounds, useBounds } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { SceneContext } from "../context";
+import * as THREE from "three";
 
 const Loader = () => (
   <Html center>
@@ -27,11 +29,15 @@ const ChairModel = ({ position, rotation }) => {
 const TableModel = ({ position }) => {
   const gltf = useLoader(GLTFLoader, "/models/table.glb");
   const clone = useMemo(() => gltf.scene.clone(), [gltf]);
+
   return <primitive object={clone} position={position} scale={4} />;
 };
+
 const Scene = () => {
-  const numTables = 20; // Total number of tables
-  const numChairsPerTable = 6;
+  const { chair } = React.useContext(SceneContext);
+
+  const numTables = 25; // Total number of tables
+  const numChairsPerTable = chair;
   const tableSpacing = 15;
   const chairRadius = 3.5;
   const columns = 4; // Number of columns in the grid
@@ -58,36 +64,54 @@ const Scene = () => {
     return positions;
   };
 
+  function SelectToZoom({ children }) {
+    const api = useBounds();
+    return (
+      <group
+        onClick={(e) => (
+          e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit()
+        )}
+        onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}
+      >
+        {children}
+      </group>
+    );
+  }
+
   return (
     <Suspense fallback={<Loader />}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} intensity={1} castShadow />
-      {Array.from({ length: numTables }).map((_, index) => {
-        const row = Math.floor(index / columns);
-        const col = index % columns;
-        const tablePosition = [col * tableSpacing, 0, row * tableSpacing];
-        const chairsPositions = generateChairPositions(
-          numChairsPerTable,
-          chairRadius,
-        );
+      <Bounds fit clip observe margin={1.2}>
+        <SelectToZoom>
+          {Array.from({ length: numTables }).map((_, index) => {
+            const row = Math.floor(index / columns);
+            const col = index % columns;
+            const tablePosition = [col * tableSpacing, 0, row * tableSpacing];
+            const chairsPositions = generateChairPositions(
+              numChairsPerTable,
+              chairRadius,
+            );
 
-        return (
-          <React.Fragment key={index}>
-            <TableModel position={tablePosition} />
-            {chairsPositions.map((chair, chairIndex) => (
-              <ChairModel
-                key={`${index}-${chairIndex}`}
-                position={[
-                  chair.position[0] + tablePosition[0],
-                  chair.position[1],
-                  chair.position[2] + tablePosition[2],
-                ]}
-                rotation={chair.rotation}
-              />
-            ))}
-          </React.Fragment>
-        );
-      })}
+            return (
+              <React.Fragment key={index}>
+                <TableModel position={tablePosition} />
+                {chairsPositions.map((chair, chairIndex) => (
+                  <ChairModel
+                    key={`${index}-${chairIndex}`}
+                    position={[
+                      chair.position[0] + tablePosition[0],
+                      chair.position[1],
+                      chair.position[2] + tablePosition[2],
+                    ]}
+                    rotation={chair.rotation}
+                  />
+                ))}
+              </React.Fragment>
+            );
+          })}
+        </SelectToZoom>
+      </Bounds>
     </Suspense>
   );
 };
@@ -99,7 +123,13 @@ const ThreeDScene = () => {
       style={{ height: "100%", cursor: "grab" }}
       shadows
     >
-      <OrbitControls enableZoom={true} enableRotate={true} />
+      <OrbitControls
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 1.75}
+        enableZoom={true}
+        enableRotate={true}
+      />
       <Scene />
     </Canvas>
   );
